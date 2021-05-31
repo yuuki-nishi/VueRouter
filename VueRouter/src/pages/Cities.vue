ec39b1e033d457565251deb59a43e3b4a286bf9f
yuuki-nishi <nishi.y.aa@m.titech.ac.jp>
2021-05-31 21:33:23 +0900

    cities

--- VueRouter/src/pages/Cities.vue
+++ VueRouter/src/pages/Cities.vue
@@ -3,7 +3,12 @@
     <h1>
         {{ cityName }}
     </h1>
-    <div v-if="cityInfo">{{ cityInfo }}</div>
+    <div v-if="cityInfo">{{ cityInfo }}
+        <div v-for="item in cityInfo" :key="item.ID">
+            名前:<router-link :to="`/citiesfromcountry/${item.ID.String}`">{{ item.name.String }}</router-link>
+            <!--ここまでが各要素の記述-->
+        </div>
+    </div>
     <div v-else>街が見つかりませんでした</div>
   </div>
 </template>
@@ -13,11 +18,11 @@ import axios from "axios";
 import { ref, onMounted } from 'vue'
 
 export default {
-  name: "City",
+  name: "Cities",
   setup(props) {
     const cityInfo = ref()
     onMounted(async () => {
-        const res = await axios.get("/api/cities/" + props.cityName)
+        const res = await axios.get("/api/citiesfromcountry/" + props.CountryID)
         cityInfo.value = res.data;
     })
     return { cityInfo }
--- VueRouter/src/pages/Countries.vue
+++ VueRouter/src/pages/Countries.vue
@@ -4,8 +4,8 @@
             {{ countryName }}
         </h1>
         <div v-if="countryInfo">
-            <div v-for="item in countryInfo" :key="item.ID">
-                名前:<router-link to=`/citiesfromcountry`>{{ item.name.String }}</router-link>
+            <div v-for="item in countryInfo" :key="item.Code">
+                名前:<router-link :to="`/citiesfromcountry/${item.Code.String}`">{{ item.name.String }}</router-link>
                 <!--ここまでが各要素の記述-->
             </div>
         </div>
@@ -19,16 +19,18 @@ import { ref, onMounted } from 'vue'
 
 export default {
   name: "Countries",
-  setup(props) {
+  setup() {
     const countryInfo = ref()
     onMounted(async () => {
         const res = await axios.get("/api/allcountry")
         countryInfo.value = res.data;
     })
-    const ShowCities = (id) =>{
+    /*
+    const Cities = (id) =>{
         axios.get("/api/citiesfromcountry/"+ id);
     };
-    return { countryInfo,ShowCities }
+    */
+    return { countryInfo/*,Cities*/ }
   },
 };
 </script>
