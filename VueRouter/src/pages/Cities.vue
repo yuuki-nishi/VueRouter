8a77658ec78df9d668360c3775d5e730e6733fe4
yuuki-nishi <nishi.y.aa@m.titech.ac.jp>
2021-06-01 00:03:13 +0900

    cities

--- VueRouter/src/pages/Cities.vue
+++ VueRouter/src/pages/Cities.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
     <h1>
-        {{ cityName }}
     </h1>
     <div v-if="cityInfo">{{ cityInfo }}
         <div v-for="item in cityInfo" :key="item.ID">
-            名前:<router-link :to="`/citiesfromcountry/${item.ID.String}`">{{ item.name.String }}</router-link>
+            名前:<router-link :to="`/citiesfromcountry/${item.ID.int}`">{{ item.Name.String }}</router-link>
             <!--ここまでが各要素の記述-->
         </div>
     </div>
-    <div v-else>街が見つかりませんでした</div>
+    <div v-else>街が見つかりませんでしたaaa</div>
   </div>
 </template>
 
@@ -19,10 +18,17 @@ import { ref, onMounted } from 'vue'
 
 export default {
   name: "Cities",
+  props: {
+    CountryID: {
+      type: String,
+      required: true
+    }
+  },
   setup(props) {
     const cityInfo = ref()
     onMounted(async () => {
         const res = await axios.get("/api/citiesfromcountry/" + props.CountryID)
+        console.log(res.data)
         cityInfo.value = res.data;
     })
     return { cityInfo }
